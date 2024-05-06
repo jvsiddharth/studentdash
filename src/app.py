ff35a11fcfcaab7704f50c94c2506dcb2f51b3bd
Siddharth Jadhav <104616265+jvsiddharth@users.noreply.github.com>
2024-05-06 16:44:57 +0530

    Update app.py

--- src/app.py
+++ src/app.py
@@ -154,4 +154,4 @@ def update_graph(selected_student, comparison_options):
 
 # Run the app
 if __name__ == '__main__':
-    app.run_server(debug=True, host='127.0.0.1', port=8050)
+    app.run_server(debug=True, host='0.0.0.0', port=8050)
